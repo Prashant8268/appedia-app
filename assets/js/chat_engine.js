de2e013886558a2a030cd62f4249b0af9bcb2573
Prashant <goden1029@gmail.com>
2023-09-06 09:35:55 +0530

    setup single chatBox

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -4,16 +4,55 @@ class ChatEngine{
         this.userEmail = userEmail;
         this.socket = io.connect('http://localhost:8000')
         if(this.userEmail){
-            console.log('check point 1')
             this.connectionHandler();
         }
     }
 
+
+
     connectionHandler(){
+
+        let self = this;
         this.socket.on('connect',()=>{
             console.log('connection establish using sockets...!')
+            self.socket.emit('join_room',{
+                user_email: self.userEmail,
+                chatroom: 'codeial'
+
+            });
+            self.socket.on('user_joined',(data)=>{
+                console.log('a user joined',data);
+            });
+
+        });
+
+        $(`#send-message`).click(function(){
+            
+            let msg = $(`#msg`).val();
+            if(msg!=''){
+                self.socket.emit('send_message',{
+                    message:msg,
+                    user_email:self.userEmail,
+                    chatroom:'codeial'
+                })
+            }
+        });
+
+        self.socket.on('receive_message',(data)=>{
+
+            let newMsg = document.createElement('li');
+            let msgType = 'other-msg';
+            if(data.user_email==self.userEmail){
+                msgType='self-msg'
+            };
+            newMsg.textContent = data.message;
+            newMsg.className=msgType;
+            document.getElementById('message-list').append(newMsg)
         })
 
 
     }
 }
+
+
+ 
--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -1,10 +1,29 @@
 
 module.exports.chatSockets = function(socketServer){
 
-    let io = require('socket.io')(socketServer);
+    let io = require('socket.io')(socketServer,{
+        cors:{
+            origin:'*'
+        }
+    });
 
     io.on('connection',function(socket){
         console.log('new connection received',socket.id,'check point 3 ');
+
+        socket.on('join_room',(data)=>{
+
+            console.log('joining request rec.',data);
+
+            socket.join(data.chatroom);
+            io.in(data.chatroom).emit('user_joined',data);
+
+        });
+
+        socket.on('send_message',(data)=>{
+            console.log('message received', data);
+            // below is for sending data to chatroom so all can receive it 
+            io.in(data.chatroom).emit('receive_message',data);
+        })
         
         socket.on('disconnect',function(){
         console.log('socket connection disconnected')
